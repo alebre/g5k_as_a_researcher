2cb4ee2b5462df3196f1b6627fadea4f3c17faa5
Adrien Lebre <alebre@yenapaquater.local>
2016-02-04 11:04:39 +0100

    Clean repo - g5k_as_a_researcher

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # g5k_as_a_researcher
-Compute the G5K citations count number, h-index ...
+
+## Installation
+
+to clone the repository, please run the following command:
+
+```
+git clone --recursive https://github.com/badock/g5k_as_a_researcher.git
+```
+
+Do not forget the **--recursive** option, otherwise a dependency may be missing.
+
+## External libraries
+
+This project is making an extensive use of the following libraries:
+
+* scholar.py ([https://github.com/ckreibich/scholar.py](https://github.com/ckreibich/scholar.py))
+* you need to install BeautifulSoup (pip install beautifulsoup)
+* You need to install selenium (pip install selenium)
+
+## Misc
+* worker_0 => alebre
+
+## usage
+* Create additional directory in data and results
+```
+mkdir ./data/$DATE ; mkdir ./results/$DATE
+```
+* Retrieve the json index of G5K  (please replace DATE by the current date in a weird format such as XXMonthYYY)
+```
+curl -o ./data/$DATE/index.json -u alebre  https://intranet.grid5000.fr/g5kbib/index.json
+python make_g5k_profile.py
+```
+Please note that the above script can last several days as it rely on several sleep of random duration between 30 and 120 seconds. Such random sleeps are mandatory to avoid being blocked by Google. 
+```
+python ./compute_g5k_index.py 
+```
+The result should be in the form fo 
+There are 768 publications
+g5k profile: citations nb 6625, h_index 66, i_index 174 (not_evaluated 23)
+
+If you want to get a textual representation of the json sorted list
+```
+cat ./results/$DATE/sorted_results.json | json_pp -t dumper > ./results/$DATE/desc-sorted-list.txt
+```
---
+++ make_g5k_profile.py
@@ -0,0 +1,201 @@
+__author__ = 'alebre'
+
+import json
+import scholar
+import sys
+import time
+import traceback
+import shutil
+import string
+from random import randint
+
+
+def make_scholar_request_fromhal(data, parsed_data):
+    result=[]
+    for internal_key in data:
+        domains = data[internal_key]['domains'] # because there is one internal level, I don't know why but ..
+        for domain_key in domains:
+            teams = domains[domain_key]['teams']
+            for team_key in teams:
+                publis = teams[team_key]['publis']
+                for publi in publis:
+                    #print publi['title']
+                    #               cmd = '/TRASH/scholar.py/scholar.py -d --phrase "%s"' % (publi['title'])
+                    #               res = os.system(cmd)
+                    #               print res
+                    if publi['type'] == 'found-in-pdf' or publi['type'] == 'found-in-collaboration':
+                        if not is_parsed(publi['title'], parsed_data+result):
+                            querier = scholar.ScholarQuerier()
+                            query = scholar.SearchScholarQuery()
+                            query.set_num_page_results(1)
+                            query.set_phrase(publi['title'])
+                            # query.set_phrase('Entropy: a consolidation manager for clusters')
+                            querier.send_query(query)
+                            try:
+                                line = {'title': publi['title'], 'citation_count': querier.articles[0]['num_citations']}
+                                # print publi['title']:querier.articles[0]['num_citations']
+                                print line
+                            except:
+                                print 'error %s' % (publi['title'])
+                                print(traceback.format_exc())
+                       #         line = {'title': publi['title'], 'citation_count': 0}
+
+                            result+=[line]
+                            if len(result) > 10:
+                                return parsed_data+result
+                            # just wait before making the new query
+                            time.sleep(randint(30,120))
+
+    return parsed_data+result
+
+
+def make_scholar_request(title):
+    citations_nb = 0
+    querier = scholar.ScholarQuerier()
+    query = scholar.SearchScholarQuery()
+    query.set_num_page_results(1)
+    query.set_phrase(title)
+    error = False
+    if (querier.send_query(query)) == True :
+        if(len(querier.articles) >= 1):
+            citations_nb = -1
+            # If the data I retrieved are correct (i.e. they correspond to the publication, I'm looking for, I update the citations_nb
+            try:
+                if (querier.articles[0]['title'].upper() == title.upper()):
+                    citations_nb = querier.articles[0]['num_citations']
+                else:
+                   print 'Title does not match('+querier.articles[0]['title'].upper()+'/'+title.upper()+')'
+                   error = True
+            except:
+                error = True 
+            if error == True:
+                citations_nb = -3
+                with open ('./errors', 'a') as outfile:
+                    outfile.write(title.encode('utf-8'));
+                    outfile.write('\n');
+                outfile.closed
+    else:
+        print 'Error 503'
+        citations_nb = -2
+    return citations_nb
+
+
+
+def get_year(raw_publis, title):
+    for raw_publi in raw_publis:
+        if raw_publi['title'] == title:
+            return raw_publi['submission_date']
+    return -1
+
+
+# worker_id = 0 or 1
+def make_scholar_request_from_filtered_json(raw_publis, parsed_data):
+
+    result=[]
+
+    for publi in raw_publis:
+        if publi['type'] == 'found-in-pdf' or publi['type'] == 'found-in-collaboration':
+            if not is_parsed(publi['title'], parsed_data+result):
+                citations_nb = make_scholar_request(publi['title'])
+                line = {'title': publi['title'], 'citation_count': citations_nb, 'year': time.strptime(get_year(raw_publis, publi['title']), "%Y-%m-%d").tm_year}
+                print line
+                result += [line]
+                if len(result) > 10:
+                    return parsed_data+result
+
+                # just wait before making the new query
+                time.sleep(randint(40, 120))
+
+    return parsed_data + result
+
+
+def is_parsed(title, data):
+    for line in data:
+        if line['title'] == title:
+            return True
+    return False
+
+
+# Retrieve all publications related to G5K (and only related to G5K)
+# data, the json file returned by g5kbib
+def filter_G5K_pub(data):
+    result=[]
+    for internal_key in data:
+        domains = data[internal_key]['domains'] # because there is one internal level, I don't know why but ..
+        for domain_key in domains:
+            teams = domains[domain_key]['teams']
+            for team_key in teams:
+                publis = teams[team_key]['publis']
+                for publi in publis:
+                    if publi['type'] == 'found-in-pdf' or publi['type'] == 'found-in-collaboration':
+                        result += [publi]
+                        print publi
+
+    return result
+
+
+def check_nonevaluated(list_publis, result_file_path):
+    i = 0  
+    for publi in list_publis:
+        if(publi['citation_count'] <= -1):
+            publi['citation_count']= make_scholar_request(publi['title'])
+
+            line = {'title': publi['title'], 'citation_count': publi['citation_count']}
+            print line
+            i += 1
+
+            if (i > 5):
+                i = 0  
+                with open (result_file_path, 'w') as outfile:
+                   json.dump(list_publis, outfile)
+                outfile.closed
+
+# wait before making the next query if the previous one succeeded.
+            if (publi['citation_count'] > -2):
+                time.sleep(randint(30, 120))
+
+def testmain():
+
+    make_scholar_request('Efficient XML query processing') 
+	
+def main(): 
+   
+    date = '17Jan2016'
+    # Get the input
+    try:
+        with open('data/'+date+'/index-g5k.json') as data_file:
+            data = json.load(data_file)
+    except:
+        with open('data/'+date+'/index.json') as data_file:
+            data_non_filtered = json.load(data_file)
+            data = filter_G5K_pub(data_non_filtered)
+            with open ('data/'+date+'/index-g5k.json', 'w') as outfile_g5k:
+                json.dump(data, outfile_g5k)
+
+    # Get the input
+    result_file_path = './results/'+date+'/result.json'
+
+    for i in range(0, randint(10, 30)):
+        try:
+            with open(result_file_path) as parsed_data_file:
+                parsed_data = json.load(parsed_data_file)
+        except:
+            parsed_data=[]
+
+        print len(parsed_data)
+
+        result = make_scholar_request_from_filtered_json(data,parsed_data)
+
+        with open (result_file_path, 'w') as outfile:
+            json.dump(result, outfile)
+        
+       # time.sleep(randint(40, 120))
+
+
+    # Check once again the non evaluated request (request that get either no publication or 503 error)
+    print 'Check non evaluated'
+    shutil.copy(result_file_path, result_file_path+'.bak')
+    check_nonevaluated(result, result_file_path)
+
+if __name__ == "__main__":
+    sys.exit(main())
